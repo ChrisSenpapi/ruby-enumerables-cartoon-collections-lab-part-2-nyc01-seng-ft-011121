0a760c7f7a1c8c46a24045ad8e276619f4fc3158
Christopher Read <christopherread22@outlook.com>
2020-12-16 19:00:40 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,9 +17,6 @@ end
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
   
-  planeteer_calls.any? { |call| call.length > 1 }
-end
-  
   calls = planeteer_calls & valid_calls
   if calls.empty?
     return nil 
