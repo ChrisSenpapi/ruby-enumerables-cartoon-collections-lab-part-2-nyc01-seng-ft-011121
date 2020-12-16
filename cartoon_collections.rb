edb3c8c46f84625ce7608ca2ac6e8f9dad2af680
Christopher Read <christopherread22@outlook.com>
2020-12-16 19:07:02 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,4 +18,5 @@ def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
   
   calls = planeteer_calls.grep(/^(Earth|Wind|Fire|Water|Heart)$/)
+  
 end
